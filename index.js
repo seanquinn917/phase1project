4aae900ca3fe90ef83c08a775ede650dd2066a37
SeanQuinn <Seanquinn917@gmail.com>
2023-04-05 09:55:52 -0400

    with added "bucket" for appended results

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ addEventListener("DOMContentLoaded", (e)=>{
     byType();
     byName();
     byState();
-    // clearList();
+    //emptyList()
     changePic()
 
 
@@ -45,8 +45,11 @@ function changePic(){
 const byName = ()=>{
     const inputForm = document.getElementById('form1')
     // console.log(inputForm)
+    
     inputForm.addEventListener('submit', (e) =>{
         // console.log( "i was clicked!")
+        let list = document.getElementById("listholder")
+        list.innerHTML = " "
         e.stopImmediatePropagation()
         e.preventDefault()
         const input = document.querySelector('input#searchByName')
@@ -55,6 +58,7 @@ const byName = ()=>{
             .then((resp)=>resp.json())
             .then((data)=> displayBreweries(data[0]));
             inputForm.reset();
+            //emptyList()
             addButton()
     })
     
@@ -81,6 +85,8 @@ function byState(){
     const inputForm = document.getElementById('form2')
     // console.log(inputForm)
     inputForm.addEventListener('submit', (e) =>{
+        let list = document.getElementById("listholder")
+        list.innerHTML = " "
         e.stopImmediatePropagation()
         e.preventDefault()
         const input = document.getElementById('searchByState')
@@ -89,7 +95,9 @@ function byState(){
         .then((resp)=>resp.json())
         .then((data)=> data.forEach(brewery => displayBreweries(brewery)))
         inputForm.reset();
+        //emptyList()
         addButton()
+       
     })
 }
 
@@ -97,6 +105,8 @@ function byType(){
     const inputForm = document.getElementById('form3')
     // console.log(inputForm)
     inputForm.addEventListener('submit', (e) =>{
+        let list = document.getElementById("listholder")
+        list.innerHTML = " "
         e.stopImmediatePropagation();
         e.preventDefault();
         const input = document.getElementById('searchByType')
@@ -106,6 +116,7 @@ function byType(){
         .then((data)=> data.forEach(brewery => displayBreweries(brewery)))
         inputForm.reset()
         addButton()
+        //emptyList()
     })
 }
 
@@ -123,29 +134,40 @@ function displayBreweries(brewery){
             ${brewery.phone}
     </div>
     `
-    document.body.appendChild(card)
+    document.getElementById("listholder").appendChild(card)
 }
 
 
 function addButton(){
  let button = document.createElement('button')
  button.innerHTML = `
- <button id='button' type ='button'>New Search!</button>
+ <button id='button' type ='button'>Clear Page</button>
  `
  const holder = document.getElementById('listholder')
 //  console.log(holder)
  holder.appendChild(button)
+ button.addEventListener('click', (e)=>{
+    let list = document.getElementById("listholder")
+    list.innerHTML = " "
+    console.log('i was clicked!')
+   
+    
  
+})
 }
 
-// function clearList(){
-//     document.getElementById('list').innerHTML=''
-// }
-
-// const butt = document.getElementById("button")
-// console.log(butt)
-
-
+// function emptyList(){
+    
+//     button.addEventListener('click', (e)=>{
+//         let list = document.getElementById("listholder")
+//         list.innerHTML = " "
+//         console.log('i was clicked!')
+       
+        
+            
+//         });
+        
+//     }
 
 
 
@@ -155,4 +177,118 @@ function addButton(){
 // // select.onclick = function(){
 // //     list.classList.toggle("open");
 // // }
+
+
+
+
+// <!-- <form>
+//             <h1>
+//             <label for="searchByName">Search by Name</label>
+//             <input id= "searchByName" type="text" placeholder="Enter Brewery Name"/>
+//             <input type="submit"/>
+//             </h1>
+//             <h2>
+//             <label for="searchByType">Search by Type</label>
+//             <input id="searchByType" type="text" placeholder="Enter Brewery Type"/>
+//             <input type="submit">
+//             </h2>
+//             <h3>
+//                 <label for="searchByState">Search by State</label>
+//                 <input id="searchByState" type="text" placeholder="Enter Brewery State"/>
+//                 <input type="submit">
+//             </h3>
+//         </form> -->
+
+// <!-- 
+        
+//         <form>
+//             <label for="Breweries for you!">Let's find your Beer!</label>
+//             <select search="search-types" id="search-types" onchange="find(this);" >
+//                 <option selected="selected">Select Option</option>
+//                 <option value="search by Name">Search By Name</option>
+//                 <option value="search by State">Search By State</option>
+//                 <option value="search by Type">Search By Type</option>
+//             </select>
+//             <div class="container">
+//                 <div class="search-bar">
+//                     <!-- <div id="select"> -->
+//                         <!-- <p>All Categories</p>
+//                         <img src="https://cdn-icons-png.flaticon.com/512/60/60995.png" alt="Drop down arrow free icon">
+//                         <ul>
+//                             <li class= "options">All Categories</li>
+//                             <li class= "options">Brewery Type</li>
+//                             <li class= "options">Brewery State</li>
+//                             <li class= "options">Brewery Name</li>
+//                         </ul> -->
+//                     <!-- </div> -->
+//                     <input type="text" placeholder="Search in all catergories">
+//                 </div>
+//             </div>
+//         </form> -->
+
+
+//         <form>
+//             <p>
+//             <label for="searchByNameS">Search by Name</label>
+//             <input id= "searchByName" type="text" placeholder="Enter Brewery Name"/>
+//             <input type="submit"/>
+//             </p>
+//             <p2>
+//             <label for="searchByType">Search by Type</label>
+//             <input id="searchByType" type="text" placeholder="Enter Brewery Type"/>
+//             <input type="submit">
+//             </p2>
+//             <br>
+//             <p3>
+//             <label for="searchByState">Search by State</label>
+//             <input id="searchByState" type="text" placeholder="Enter Brewery State"/>
+//             <input type="submit">
+//             </p3>
+//         </form>
+
+
+
+
+
+//          <form>
+//             <label for="Breweries for you!">Let's find your Beer!</label>
+//             <select search="search-types" id="search-types" onchange="find(this);" >
+//                 <option selected="selected">Select Option</option>
+//                 <option value="search by Name">Search By Name</option>
+//                 <option value="search by State">Search By State</option>
+//                 <option value="search by Type">Search By Type</option>
+//             </select>
+//             <div class="container">
+//             </div>
+//         </form>
+
+        
+
+// // function find(){
+//     const search = document.getElementById("search-types")
+//     //console.log(search)
+//     search.addEventListener('change',(e)=>{
+//         e.preventDefault();
+//         if(e.target.value === "search by Name"){
+//             console.log(e.target.value);
+//         } else if(e.target.value === "search by Type"){
+//         console.log(e.target.value);
+//         }
+//          else if(e.target.value === "search by State"){
+//             console.log(e.target.value);
+//         }
+//     }
+//     ) 
+// }
+   
+// const createSearchBar = (e)=> {
+//     let searchBar = document.createElement("li")
+//         searchBar.class = 'searching'
+//         searchBar.innerHTML = `
+//         <div class = 'content'>
+        
+
+//         `
+//         document.body.appendChild(searchBar)
+//     }
 
